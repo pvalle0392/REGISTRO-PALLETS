8e464b172cb0e0118da7d7a3a88a30f9c90f4427
pvalle0392 <pvalle0392@gmail.com>
2022-08-19 18:42:15 -0500

    subir a produccion

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var app = require('./app');
-var port = 3700;
+// var port = 3700;
+const port = proccess.env.PORT || 3700;
 
 app.listen(port,()=>{
     console.log('servidor conectado al puerto', port);
